21b74cccaeff1b96faec3a55af9a096f3bfb9598
Alexandre Barros <103068697+alexandre21ssouza@users.noreply.github.com>
2025-04-01 21:28:38 +0000

    Desafio nível novato completo

--- xadrez.c
+++ xadrez.c
@@ -6,16 +6,55 @@
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    /* Sugestão: Declare variáveis constantes para representar o número de casas que 
+    cada peça pode se mover.*/
+    int bispo_cor_preta = 0;
+    int torre_cor_preta = 1;
+    int rainha_cor_preta = 1;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    /* Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo
+     em diagonal.*/
+
+     printf("BEM VINDO AO XADREZ\n\n\n");
+     
+     printf("Movimento do Bispo cor Preta\n\n");
+     for (int bispo_cor_preta = 0; bispo_cor_preta < 5; bispo_cor_preta++){
+        printf("Bispo se movendo para: \n");
+        printf("Cima\n");
+        printf("Direita\n");
+        /* code */
+     }
+     
+     printf("\n\n\n");
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    /* Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre
+     para a direita.*/
+     printf("Movimento da Torre cor Preta\n\n");
+
+     while (torre_cor_preta <= 5){
+      
+      printf("Torre se movendo para: \n");
+      printf("Direita\n");
+      torre_cor_preta ++;
+   }
+     
+          
+     printf("\n\n\n");
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    /* Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha
+     para a esquerda.*/
+     printf("Movimento da Rainha cor Preta\n\n");
+
+     do{
+      printf("Rainha se movendo para: \n");
+      printf("Esquerda\n");
+      rainha_cor_preta ++;
+     } while (rainha_cor_preta <= 8);
+
+     printf("\n\n\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
